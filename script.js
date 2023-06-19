e2b87da6f6b8aa0dd2f6a7139e87d64a98f89f9a
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-19 15:49:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function volume_sphere() {
     //Write your code here
+	const radius = document.getElementById("radius");
+	if(radius==0){
+		alert("invalid input");
+	}
+	const vol = radius.value*radius.value;
+	document.getElementById("volume").value=vol;
+	
   
 } 
 
